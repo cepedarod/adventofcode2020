154b3e578dad7e136c1a7d7e9834e36c203ebf4d
rodrigo <rcepedaw@cisco.com>
2020-12-11 14:29:53 -0600

    cleaned up code for challenge 1

--- challenge_1.py
+++ challenge_1.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 
 numbers = []
-
-
 with open('input.txt', 'r') as f:
     numbers = f.readlines()
 
-while True:
-    leftover = numbers[1:]
-    it_1 = 1
-    for n in numbers:
-        others = leftover[it_1:]
-        it_2 = 1
-        for i in leftover:
-            others = leftover[it_2:]
-            for ii in others:
-                if int(n) + int(i) + int(ii) == 2020:
-                    print(int(n) * int(i) * int(ii))
-                    return
-            it_2 += 1
-        it_1 += 1
+still_looking = True
+while still_looking:
+    for i, num_1 in enumerate(numbers):
+        for ii, num_2 in enumerate(numbers[i+1:]):
+            for num_3 in numbers[i+ii+1:]:
+                if int(num_1) + int(num_2) + int(num_3) == 2020:
+                    print(int(num_1) * int(num_2) * int(num_3))
+                    still_looking = False
+                    break
+            if still_looking == False: break
+        if still_looking == False: break
